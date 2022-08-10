0a3a01b1fb7bb901d91666be91f2ccc26a701357
ollie <lieshoff@gmx.de>
2022-08-11 00:10:07 +0200

    Reformatted.

--- jrxmlreader.go
+++ jrxmlreader.go
@@ -2,32 +2,32 @@ package main
 
 import (
 	"encoding/xml"
-    "fmt"
+	"fmt"
 	"io/ioutil"
 	"log"
-    "os"
+	"os"
 )
 
 type JasperReport struct {
-	XMLName      xml.Name     `xml:"jasperReport"`
-	Fields       []Field      `xml:"field"`
-	Parameters   []Parameter  `xml:"parameter"`
-	Variables    []Variable   `xml:"variable"`
+	XMLName    xml.Name    `xml:"jasperReport"`
+	Fields     []Field     `xml:"field"`
+	Parameters []Parameter `xml:"parameter"`
+	Variables  []Variable  `xml:"variable"`
 }
 
 type Field struct {
-	XMLName    xml.Name     `xml:"field"`
-	Name       string       `xml:"name,attr"`
+	XMLName xml.Name `xml:"field"`
+	Name    string   `xml:"name,attr"`
 }
 
 type Parameter struct {
-	XMLName    xml.Name     `xml:"parameter"`
-	Name       string       `xml:"name,attr"`
+	XMLName xml.Name `xml:"parameter"`
+	Name    string   `xml:"name,attr"`
 }
 
 type Variable struct {
-	XMLName    xml.Name     `xml:"variable"`
-	Name       string       `xml:"name,attr"`
+	XMLName xml.Name `xml:"variable"`
+	Name    string   `xml:"name,attr"`
 }
 
 func ReadJasperReport(fileName string) *JasperReport {
@@ -43,22 +43,21 @@ func ReadJasperReport(fileName string) *JasperReport {
 	return report
 }
 
-
-func main () {
+func main() {
 	log.Println("Started JasperReports Cleaner ================================================================================================")
 	if len(os.Args) < 2 {
 		log.Fatalln("Missing JRXML file name")
 		return
 	}
 	var report = ReadJasperReport(os.Args[1])
-    for _, field := range report.Fields {
-        fmt.Println("$F{" + field.Name + "}")
-    }
-    for _, parameter := range report.Parameters {
-        fmt.Println("$P{" + parameter.Name + "}")
-    }
-    for _, variable := range report.Variables {
-        fmt.Println("$V{" + variable.Name + "}")
-    }
+	for _, field := range report.Fields {
+		fmt.Println("$F{" + field.Name + "}")
+	}
+	for _, parameter := range report.Parameters {
+		fmt.Println("$P{" + parameter.Name + "}")
+	}
+	for _, variable := range report.Variables {
+		fmt.Println("$V{" + variable.Name + "}")
+	}
 	log.Println("Finished JasperReports Cleaner ===============================================================================================")
 }
