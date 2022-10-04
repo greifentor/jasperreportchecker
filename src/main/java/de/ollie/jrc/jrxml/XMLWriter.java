4ad4d54aa3f5f69b0ed73d12bc00d99857061075
Oliver Lieshoff <o.lieshoff@schuetze-consulting.ag>
2022-10-04 16:15:58 +0200

    Small progress for the XML writer.

--- src/main/java/de/ollie/jrc/jrxml/XMLWriter.java
+++ src/main/java/de/ollie/jrc/jrxml/XMLWriter.java
@@ -7,6 +7,14 @@ import de.ollie.jrc.xml.model.XMLNode;
 public class XMLWriter {
 
 	public void write(XMLNode rootNode, PrintStream printStream) {
+		if (rootNode == null) {
+			return;
+		}
+		printStream.print("<" + rootNode.getName() + ">");
+		for (XMLNode node : rootNode.getNodes()) {
+			write(node, printStream);
+		}
+		printStream.print("</" + rootNode.getName() + ">");
 	}
 
 }
--- src/test/java/de/ollie/jrc/jrxml/XMLWriterTest.java
+++ src/test/java/de/ollie/jrc/jrxml/XMLWriterTest.java
@@ -1,11 +1,19 @@
 package de.ollie.jrc.jrxml;
 
-import java.io.PrintStream;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verifyNoInteractions;
 
+import java.io.PrintStream;
+import java.util.List;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import de.ollie.jrc.xml.model.XMLNode;
 
 @ExtendWith(MockitoExtension.class)
 public class XMLWriterTest {
@@ -15,5 +23,34 @@ public class XMLWriterTest {
 
 	@InjectMocks
 	private XMLWriter unitUnderTest;
+
+	@Nested
+	class TestsOfMethod_write_XMLNode_PrintStream {
+
+		@Test
+		void throwsAnException_passingANullValueAsPrintStream() {
+			XMLNode xmlNode = new XMLNode();
+			assertThrows(NullPointerException.class, () -> unitUnderTest.write(xmlNode, null));
+		}
+
+		@Test
+		void writesNothing_passingANullValueAsXMLNode() {
+			// Run
+			unitUnderTest.write(null, printStream);
+			// Check
+			verifyNoInteractions(printStream);
+		}
+
+		@Test
+		void t() {
+			unitUnderTest
+					.write(
+							new XMLNode()
+									.setName("nodes")
+									.setNodes(List.of(new XMLNode().setName("node"), new XMLNode().setName("node"))),
+							System.out);
+		}
+
+	}
 
 }
